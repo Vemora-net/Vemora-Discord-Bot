31b42ebc327bb8ae90bd7f7bdfd561916cd33f9d
Joshua Hatzinger <joshuahatzinger@outlook.de>
2019-11-28 11:33:52 +0100

    added new files to .gitignore

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const {
 } = require('discord.js');
 var MongoClient = require('mongodb').MongoClient;
 const client = new Client();
-const fs = require("fs");
-
-const io = require("socket.io");
+const http = require("http");
+var server = http.createServer().listen(2321);
+const io = require("socket.io")(server);
 const {guildid, user_role, token, server_banner_role_2, server_banner_role_1, welcome_id, bye_id} = require("./config");
-var guild, welcome_channel;
+var guild, welcome_channel, bye_channel;
 
 //Ready
 
@@ -20,6 +20,7 @@ client.on("ready", () =>{
         }});
     guild = client.guilds.find(val => val.id === guildid);
     welcome_channel = guild.channels.find(val => val.id === welcome_id);
+    bye_channel = guild.channels.find(val => val.id === bye_id);
     client.setTimeout(() =>{
         setStandardRoles();
         client.user.setStatus("online");
@@ -31,12 +32,27 @@ client.on("ready", () =>{
             setStandardRoles();
         }, 60000);
     }, 3000);
+    io.on('connection', (socket) => {
+        socket.on('move', (user, channel) =>{
+
+        });
+        socket.on('chat', (user, message) =>{
+            var auser = guild.members.find(val => val.id === user);
+            if(auser){
+                auser.send(message);
+                socket.send(true);
+            }
+        });
+    });
+
 });
 
 client.on("guildMemberAdd", (member) =>{
-    welcome_channel.send("Welcome " + member.user + "!")
+    welcome_channel.send("Welcome " + member.user + "!");
 });
-
+client.on("guildMemberRemove", (member) =>{
+    bye_channel.send("Say goodbye to " + member.user.tag + "!");
+});
 function setStandardRoles(){
     guild.members.forEach((member) =>{
         if(!hasRole(user_role, member)) member.addRole(user_role);
